c2c157399680493e9fc0741a2907f642f48bbe29
Elizabeth <foxcapade@gmail.com>
2022-08-03 10:21:22 -0400

    updates

--- lib/src/main/java/org/veupathdb/lib/jaxrs/raml/multipart/utils/CappedOutputStream.kt
+++ lib/src/main/java/org/veupathdb/lib/jaxrs/raml/multipart/utils/CappedOutputStream.kt
@@ -3,6 +3,12 @@ package org.veupathdb.lib.jaxrs.raml.multipart.utils
 import jakarta.ws.rs.BadRequestException
 import java.io.OutputStream
 
+/**
+ * Capped Size Output Stream
+ *
+ * A simple output stream wrapper that throws an exception if more than the
+ * specified max number of bytes is read.
+ */
 class CappedOutputStream(
   private val maxBytes: Int,
   private val stream: OutputStream
